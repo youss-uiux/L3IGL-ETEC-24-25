cffe601c8556dd43a9e63b6ccd68feeb01880271
youss-uiux <youssif.aboubacar@outlook.fr>
2025-07-26 11:22:22 +0200

    Implement reactive form for contact page and add showValue method

--- first-angular/src/app/Pages/contact-page/contact-page.component.ts
+++ first-angular/src/app/Pages/contact-page/contact-page.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.sass'
 })
@@ -17,6 +20,12 @@ export class ContactPageComponent {
     this.router.navigate(['home']);
     // This would typically use a router service, e.g., this.router.navigate(['home']);
   }
+
+  showValue():void{
+    alert(`Name: ${this.name.value}`);
+  }
+
+  name = new FormControl('');
 
 }
 export default ContactPageComponent;
