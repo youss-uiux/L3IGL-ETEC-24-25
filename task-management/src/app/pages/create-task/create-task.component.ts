fda5b3b7ce74810a5d7fbbd1bd6236c9c3cf6535
youss-uiux <youssif.aboubacar@outlook.fr>
2025-07-28 11:13:52 +0200

    Add Tailwind CSS integration and implement reactive form for task creation

--- task-management/src/app/pages/create-task/create-task.component.ts
+++ task-management/src/app/pages/create-task/create-task.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule,FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-task',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-task.component.html',
   styleUrl: './create-task.component.scss'
 })
 export class CreateTaskComponent {
 
+  taskForm = new FormGroup({
+    title: new FormControl('', ),
+    description: new FormControl('', Validators.minLength(20) ),
+    priority: new FormControl('', ),
+    dueDate: new FormControl('', ),
+  });
+
+  onSubmit(): void {
+    if (this.taskForm.valid) {
+      alert(`Task created: ${JSON.stringify(this.taskForm.value)}`);
+    } else {
+      alert('Form is invalid');
+    }
+  }
 }
--- task-management/src/app/pages/home/home.component.ts
+++ task-management/src/app/pages/home/home.component.ts
@@ -1,15 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   router=inject(Router);
 
+  tasks: { title: string; description: string }[] = [
+    { title: 'Task 1', description: 'Description for task 1' },
+    { title: 'Task 2', description: 'Description for task 2' },
+    { title: 'Task 3', description: 'Description for task 3' },
+    { title: 'Task 4', description: 'Description for task 4' },
+    { title: 'Task 5', description: 'Description for task 5' }
+  ];
   createTask(): void {
     console.log('Create Task button clicked');
     this.router.navigate(['/create-task']);
