83082cc6ba3a506c3b06372c342fe68c30cb4618
youss-uiux <youssif.aboubacar@outlook.fr>
2025-07-28 10:21:48 +0200

    Initialize application structure with basic components and routing setup

---
+++ task-management/src/app/pages/create-task/create-task.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-task',
+  imports: [],
+  templateUrl: './create-task.component.html',
+  styleUrl: './create-task.component.scss'
+})
+export class CreateTaskComponent {
+
+}
---
+++ task-management/src/app/pages/details-task/details-task.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-task',
+  imports: [],
+  templateUrl: './details-task.component.html',
+  styleUrl: './details-task.component.scss'
+})
+export class DetailsTaskComponent {
+
+}
