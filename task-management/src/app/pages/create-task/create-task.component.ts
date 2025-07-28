957daab69f1895872ef6780962109e37669388f4
youss-uiux <youssif.aboubacar@outlook.fr>
2025-07-28 12:21:27 +0200

    Implement task creation functionality with form validation and service integration

--- task-management/src/app/pages/create-task/create-task.component.ts
+++ task-management/src/app/pages/create-task/create-task.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule,FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { Task, TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-create-task',
@@ -9,15 +10,27 @@ import { FormsModule,FormControl, ReactiveFormsModule, FormGroup, Validators } f
 })
 export class CreateTaskComponent {
 
+  taskService=inject(TaskService);
   taskForm = new FormGroup({
     title: new FormControl('', ),
-    description: new FormControl('', Validators.minLength(20) ),
+    description: new FormControl('',  ),
     priority: new FormControl('', ),
     dueDate: new FormControl('', ),
+    completed: new FormControl(false, ),
   });
-
+  
   onSubmit(): void {
     if (this.taskForm.valid) {
+      this.taskService.addTask(this.taskForm.value as unknown as Omit<Task, 'id' | 'createdAt'>).subscribe({
+        next: (response) => {
+          console.log('Task created successfully:', response);
+          alert('Task created successfully');
+        },
+        error: (error) => {
+          console.error('Error creating task:', error);
+          alert('Error creating task');
+        }
+      });
       alert(`Task created: ${JSON.stringify(this.taskForm.value)}`);
     } else {
       alert('Form is invalid');
